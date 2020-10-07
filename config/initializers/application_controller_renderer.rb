4e648fcdf99b13d9d74a3dabf1a8d3ad7157f9b9
EC2 Default User <wada.rio1001@gmail.com>
2020-10-07 15:13:52 +0000

    [Finish]3_画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9ceddece6ea3b4f5f54e348a6bd7acf9d69e07d8ff6030a3545d6a6540299ae5c84f67579d92ba92419d75190e6213b3b95044675f53d147581b00f2f1ef0130'
